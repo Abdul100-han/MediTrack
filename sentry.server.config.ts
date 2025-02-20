e74c7e608e079da8fd3747e6bb83819d5ab3edd3
Abdul100-han <abdulmuhd857@gmail.com>
2025-02-20 19:03:21 +0100

    error fixing and dubugging beofere deployement, also implement sentry functionalaity

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f8dd0168d9900b1ee487135f759f962f@o4508560647520256.ingest.us.sentry.io/4508846842118144",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
